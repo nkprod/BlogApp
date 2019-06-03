cac2f35acb2acc2a3bc4c59bec6652e0d155a4e1
Nurlybek Karshiga <nurlybek.karshyga@gmail.com>
2019-06-02 20:52:48 -0400

    Added SQLAlchemy
    Added SQLAlchemy and written a structure for it in python

--- application.py
+++ application.py
@@ -1,9 +1,36 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'b7aa6f35382eaaabc1363fb6f66fcba9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return "User ('{}', '{}', '{}') ".format(self.username, self.email, self.image_file)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return "Post ('{}'), ('{}')".format(self.title, self.date_posted)
 
 @app.route("/")
 @app.route("/home")
@@ -15,15 +42,24 @@ def about():
     return render_template("goodbye.html", title = 'About')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
     return render_template("register.html", title = 'Register', form=form)
 
 
-@app.route("/register")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'nkprod@apple.com' and form.password.data == '123456789':
+            flash("You have successfully logged in", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login Unsuccessful. Please check the username or password.", "danger")
     return render_template("login.html", title = 'Login', form=form)
 
 
